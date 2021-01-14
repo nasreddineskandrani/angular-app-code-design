879769e69475dd756ece24287d8c42602f3450d8
unknown <nasreddine.skandrani@gmail.com>
2021-01-14 15:15:23 -0500

    add global http error handling

--- src/app/+state/app.effects.ts
+++ src/app/+state/app.effects.ts
@@ -1,5 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Actions, createEffect } from '@ngrx/effects';
+import { filter } from 'rxjs/operators';
 
 @Injectable()
 export class AppEffects {
+
+    globalHttpErrorHandling$ = createEffect(() => {
+        return this.actions$.pipe(
+            filter(a => {
+                if (a.type.includes('ERROR')) {
+                    alert('ERROR');
+                    return true;
+                }
+                return false;
+            })
+        );
+    }, { dispatch: false });
+
+    constructor(private actions$: Actions) {}
 }
--- src/app/features/games/sales/+state/sales.effects.ts
+++ src/app/features/games/sales/+state/sales.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { catchError, filter, map, switchMap } from 'rxjs/operators';
 import { SalesApiService } from 'src/app/api/sales-api.service';
 import { FetchGameHistory, FetchHistoryError, FetchHistorySuccess } from '../../+shared/chart-history/+state/chart-history.actions';
@@ -14,13 +14,20 @@ export class SalesEffects {
             return action.id === 'sales';
         }),
         switchMap(action => {
+
             return this.salesApiService.get(action.startDate, action.endDate).pipe(
-                map(res => FetchHistorySuccess({
-                    id: action.id,
-                    startDate: action.startDate,
-                    endDate: action.endDate,
-                    data: res
-                })),
+                map(res => {
+                    const error = localStorage.getItem('fakeError');
+                    if (error) {
+                        throw({msg: 'wouahhahaha'});
+                    }
+                    return FetchHistorySuccess({
+                        id: action.id,
+                        startDate: action.startDate,
+                        endDate: action.endDate,
+                        data: res
+                    });
+                }),
                 catchError(error => of(FetchHistoryError({ id: '' })))
             );
         })
